70b62295a5a4fc7ac067eb6de0786b018170d17d
Bioneisme <bioneisme@gmail.com>
2022-06-28 20:59:44 +0600

    removed unnecessary components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -18,13 +18,7 @@ function Nav() {
             return (
                 <ul className="flex-row">
                     <li className="mx-1">
-                        <Link to="/orderHistory">
-                            Order History
-                        </Link>
-                    </li>
-                    <li className="mx-1">
-                        {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-                        <a href="/" onClick={() => Auth.logout()}>
+                        <a href="/" onClick={() => Auth.logout()} >
                             Logout
                         </a>
                     </li>
@@ -58,7 +52,7 @@ function Nav() {
             <h1>
                 <Link to="/">
                     <span role="img" aria-label="shopping bag">🛍️</span>
-                    -Shop-Shop
+                    MCQ Tests
                 </Link>
             </h1>
 
